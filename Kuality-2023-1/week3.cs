2d08e60f360d58391746324cca3307cdf1e8f16b
SeoHyeon Park <dami9722@korea.ac.kr>
2023-03-16 18:31:32 +0900

    Initial commit

---
+++ Kuality-2023-1/week3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuality_2023_1
+{
+    internal class week3
+    {
+    }
+}
